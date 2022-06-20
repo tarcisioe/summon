f77b5bcf5924fdd9dcddc9649c201c78a5fe668d
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2022-06-20 02:42:24 -0300

    Remove another unused import.

--- tests/test_tasks/test_single_result_task/tasks.py
+++ tests/test_tasks/test_single_result_task/tasks.py
@@ -1,9 +1,9 @@
-from typing import List
-
+"""Application with a task that returns a single `Result`."""
 from summon.execute import CommandError, CommandSuccess, Result
 from summon.tasks import task_with_result
 
 
 @task_with_result
 def test_task(fail: bool = False) -> Result:
+    """Task that returns a single `Result`."""
     return CommandError("dummy2", 3) if fail else CommandSuccess("dummy2")
